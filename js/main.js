761506e10e430d3a0df4377621db4d0ed3471006
maknetaRo <makneta@gmail.com>
2020-04-05 01:49:02 +0200

    feat: Add testimonials, services, contact sections and build navigation with sticky navbar in javascript

--- js/main.js
+++ js/main.js
@@ -18,11 +18,28 @@
  *
  */
 
+const navbar = document.querySelector("navbar__menu");
+const ulList = document.getElementById("navbar__list");
+const allSections = document.getElementsByTagName("section");
+const allSectionsList = Array.from(allSections);
+
 /**
  * End Global Variables
  * Start Helper Functions
  *
  */
+allSectionsList.forEach((item) => {
+  let elem = document.createElement("li");
+  let aTag = document.createElement("a");
+  let url = `#${item.id}`;
+
+  let urlElem = url;
+  aTag.href = urlElem;
+  aTag.textContent = item.dataset.nav;
+
+  elem.append(aTag);
+  ulList.appendChild(elem);
+});
 
 /**
  * End Helper Functions
@@ -45,5 +62,17 @@
 // Build menu
 
 // Scroll to section on link click
+window.onscroll = function () {
+  stickyNavbar();
+};
+const sticky = navbar.offSetTop;
+
+function stickyNavbar() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
 
 // Set sections as active
